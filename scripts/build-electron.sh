46433e1598577d602db7ffa1659db4a5b69d849d
Brad Pitcher <bradpitcher@gmail.com>
2024-07-08 15:14:55 -0500

    fix: use the --workspace param appropriately

--- scripts/build-electron.sh
+++ scripts/build-electron.sh
@@ -22,25 +22,24 @@ find packages/bruno-electron/web -name '*.map' -type f -delete
 # TODO: Get arch to pass to electron-builder
 ARCH=arm64
 
-cd packages/bruno-electron
 if [ "$1" == "snap" ]; then
   echo "Building snap distribution"
-  npm run dist:snap -- --$ARCH
+  npm run dist:snap --workspace=packages/bruno-electron -- --$ARCH
 elif [ "$1" == "mac" ]; then
   echo "Building mac distribution"
-  npm run dist:mac -- --$ARCH
+  npm run dist:mac --workspace=packages/bruno-electron -- --$ARCH
 elif [ "$1" == "win" ]; then
   echo "Building windows distribution"
-  npm run dist:win -- --$ARCH
+  npm run dist:win --workspace=packages/bruno-electron -- --$ARCH
 elif [ "$1" == "deb" ]; then
   echo "Building debian distribution"
-  npm run dist:deb -- --$ARCH
+  npm run dist:deb --workspace=packages/bruno-electron -- --$ARCH
 elif [ "$1" == "rpm" ]; then
   echo "Building rpm distribution"
-  npm run dist:rpm -- --$ARCH
+  npm run dist:rpm --workspace=packages/bruno-electron -- --$ARCH
 elif [ "$1" == "linux" ]; then
   echo "Building linux distribution"
-  npm run dist:linux -- --$ARCH
+  npm run dist:linux --workspace=packages/bruno-electron -- --$ARCH
 else
   echo "Please pass a build distribution type"
 fi
